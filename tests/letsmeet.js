85f46f3c71a4fd8d9bc3c32959e3cf410e8799d0
Mészáros Mihály <misi@majd.eu>
2020-12-04 18:11:49 +0100

    midify letsmeet.no to letsmeet.hu

--- tests/letsmeet.js
+++ tests/letsmeet.js
@@ -2,7 +2,7 @@ module.exports = {
   'letsmeet.hu test': function (browser) {
     const randomId = Math.random().toString(36).substring(2, 5)
     browser
-      .url('https://letsmeet.no/nightwatch')
+      .url('https://letsmeet.hu/nightwatch')
       .waitForElementVisible('body', 15000)
       .assert.titleContains('Multiparty Meeting')
       .waitForElementVisible('#displayname', 15000)
@@ -10,7 +10,7 @@ module.exports = {
       .setValue('#displayname', `-${randomId}`)
       .waitForElementVisible('button.MuiButtonBase-root:nth-child(2)')
       .click('button.MuiButtonBase-root:nth-child(2)')
-      .waitForElementVisible('.webcam', 15000)
+      .waitForElementVisible('.webcam', 150000)
       .waitForMediaPlaybackReady('.isMe', 10000)
       .pause(30000)
       .saveScreenshot(`tmp/screencapture-${randomId}.png`)
